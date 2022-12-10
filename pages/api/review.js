a518b3e103c84b5b6381162307ea3cecdc2f0da7
Lowe_Lowing <lowe.lowing@gmail.com>
2022-12-10 23:23:17 +0100

    res

--- pages/api/review.js
+++ pages/api/review.js
@@ -2,12 +2,12 @@
 
 export default async function handler(req, res) {
   const { id, client, newReview } = JSON.parse(req.body);
-  const res = await client
+  const result = await client
     .patch(
       `${id}?apiKey=${"skrYGo6ERATsWBeFJHvCPsCb7rbVB9sydxjpsymrk55zj5SRIDRQqjmPrk489oe8vuXltTE782Iyx9aRdLPluBEXaJdu876hxTgdZKDyGh5hpM1VkhZFpvy1nlrhdLzQEYtEvmIHejdSrkABc7YHTKNLN8mUZbXpdlxlszl6f3RPAmJYnm1T"}`
     )
     .setIfMissing({ reviews: [] })
     .append("reviews", [newReview])
     .commit({ autoGenerateArrayKeys: true });
-  res.status(200).json(res);
+  res.status(200).json(result);
 }
